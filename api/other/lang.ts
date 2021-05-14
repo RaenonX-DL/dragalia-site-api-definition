ff3ea68f6ffd7bbf4d7248dc1d412822b2b533ef
RaenonX <raenonx0710@gmail.com>
2021-05-14 07:09:01 -0500

    ADD - Util function to check if the language is supported
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- api/other/lang.ts
+++ api/other/lang.ts
@@ -4,8 +4,12 @@ export enum SupportedLanguages {
   JP = 'jp',
 }
 
-export const SupportedLanguageNames: {[lang in SupportedLanguages]: string} = {
+export const SupportedLanguageNames: { [lang in SupportedLanguages]: string } = {
   [SupportedLanguages.CHT]: 'CHT - 繁體中文',
   [SupportedLanguages.EN]: 'EN - English',
   [SupportedLanguages.JP]: 'JP - 日本語',
+};
+
+export const isSupportedLang = (lang: string): lang is SupportedLanguages => {
+  return Object.values(SupportedLanguages).some((supported) => supported === lang);
 };
