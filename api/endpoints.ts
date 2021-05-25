2e78bf1035583d827b8196895d32652458a9e624
RaenonX <raenonx0710@gmail.com>
2021-05-25 17:03:22 -0500

    IMP - Change API EPs to `enum`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -1,30 +1,31 @@
 /**
  * Class for API endpoints.
  */
-export class ApiEndPoints {
-  static ROOT = '/';
+export enum ApiEndPoints {
+  ROOT = '/',
 
-  static PAGE_META_GENERAL = '/meta/general';
-  static PAGE_META_POST = '/meta/post';
+  PAGE_META_GENERAL = '/meta/general',
+  PAGE_META_POST = '/meta/post',
 
-  static USER_LOGIN = '/user/login';
-  static USER_IS_ADMIN = '/user/is-admin';
+  USER_LOGIN = '/user/login',
+  USER_IS_ADMIN = '/user/is-admin',
 
-  static POST_QUEST_PUBLISH = '/posts/quest/publish';
-  static POST_QUEST_LIST = '/posts/quest';
-  static POST_QUEST_GET = '/posts/quest/get';
-  static POST_QUEST_EDIT = '/posts/quest/edit';
-  static POST_QUEST_ID_CHECK = '/posts/quest/id-check';
+  POST_QUEST_PUBLISH = '/posts/quest/publish',
+  POST_QUEST_LIST = '/posts/quest',
+  POST_QUEST_GET = '/posts/quest/get',
+  POST_QUEST_EDIT = '/posts/quest/edit',
+  POST_QUEST_ID_CHECK = '/posts/quest/id-check',
 
-  static POST_ANALYSIS_PUBLISH_CHARA = '/posts/analysis/publish/chara';
-  static POST_ANALYSIS_PUBLISH_DRAGON = '/posts/analysis/publish/dragon';
-  static POST_ANALYSIS_LOOKUP = '/posts/analysis/lookup';
-  static POST_ANALYSIS_GET = '/posts/analysis/get';
-  static POST_ANALYSIS_EDIT_CHARA = '/posts/analysis/edit/chara';
-  static POST_ANALYSIS_EDIT_DRAGON = '/posts/analysis/edit/dragon';
-  static POST_ANALYSIS_ID_CHECK = '/posts/analysis/id-check';
+  POST_ANALYSIS_PUBLISH_CHARA = '/posts/analysis/publish/chara',
+  POST_ANALYSIS_PUBLISH_DRAGON = '/posts/analysis/publish/dragon',
+  POST_ANALYSIS_LOOKUP = '/posts/analysis/lookup',
+  POST_ANALYSIS_LOOKUP_LANDING = '/posts/analysis/lookup/landing',
+  POST_ANALYSIS_GET = '/posts/analysis/get',
+  POST_ANALYSIS_EDIT_CHARA = '/posts/analysis/edit/chara',
+  POST_ANALYSIS_EDIT_DRAGON = '/posts/analysis/edit/dragon',
+  POST_ANALYSIS_ID_CHECK = '/posts/analysis/id-check',
 
   // These shouldn't be used in any cases besides CI
-  static NOT_EXISTS = '/404';
-  static ERROR_TEST = '/500';
+  NOT_EXISTS = '/404',
+  ERROR_TEST = '/500',
 }
