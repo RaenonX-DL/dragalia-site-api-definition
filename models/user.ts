f084c7640cf7a4b24b09c12da8cecf93cb0cc548
RaenonX <raenonx0710@gmail.com>
2021-06-14 15:58:25 -0500

    ADD - User document definitions
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- models/user.ts
+++ models/user.ts
@@ -7,8 +7,28 @@ export const AUTH_USER_COLLECTION = 'users';
 
 export type User = {
   id: ObjectId,
+  isAdmin: boolean,
   createdAt: Date,
   updatedAt: Date,
-  isAdmin: boolean,
   adsFreeExpiry?: Date,
+}
+
+export enum UserDocumentKey {
+  name = 'name',
+  email = 'email',
+  image = 'image',
+  isAdmin = 'isAdmin',
+  createdAt = 'createdAt',
+  updatedAt = 'updatedAt',
+  adsFreeExpiry = 'adsFreeExpiry',
+}
+
+export type UserDocument = {
+  [UserDocumentKey.name]: string,
+  [UserDocumentKey.email]: string,
+  [UserDocumentKey.image]: string,
+  [UserDocumentKey.isAdmin]: boolean,
+  [UserDocumentKey.createdAt]: Date,
+  [UserDocumentKey.updatedAt]: Date,
+  [UserDocumentKey.adsFreeExpiry]?: Date,
 }
