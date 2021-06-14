72e2d58bcf64afd199cfc415c2d31a3dbe36cf27
RaenonX <raenonx0710@gmail.com>
2021-06-14 15:39:39 -0500

    ADD - User db and collection names
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- models/user.ts
+++ models/user.ts
@@ -1,6 +1,10 @@
 import {ObjectId} from 'mongodb';
 
 
+export const AUTH_DB = 'nextauth';
+
+export const AUTH_USER_COLLECTION = 'users';
+
 export type User = {
   id: ObjectId,
   createdAt: Date,
