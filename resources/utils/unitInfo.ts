682291f6773d4e3039f9951bed407364ef5d3e24
RaenonX <raenonx0710@gmail.com>
2021-06-24 02:08:24 -0500

    ADD - Function to generate unit info name map
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- resources/utils/unitInfo.ts
+++ resources/utils/unitInfo.ts
@@ -1,3 +1,5 @@
+import {UnitNameRefData} from '../../api/data/unitNameRef/elements';
+import {SupportedLanguages} from '../../api/other/lang';
 import {UnitType} from '../../api/other/unit';
 import {CharaInfo} from '../types/charaInfo';
 import {DragonInfo} from '../types/dragonInfo';
@@ -15,4 +17,26 @@ export const toUnitInfoMap = <K extends number | string>(
   data.push(...dragonInfo.map((data) => ({...data, type: UnitType.DRAGON})));
 
   return new Map(data.map((info) => [getKey(info), info]));
+};
+
+export const toUnitInfoNameMap = (
+  unitInfoIdMap: UnitInfoMap<number>,
+  lang: SupportedLanguages,
+  unitNameRef: UnitNameRefData,
+): UnitInfoMap<string> => {
+  const ret = new Map<string, UnitInfoData>();
+
+  for (const unitInfo of unitInfoIdMap.values()) {
+    ret.set(unitInfo.name[lang], unitInfo);
+  }
+
+  Object.entries(unitNameRef).forEach(([name, unitId]) => {
+    const unitInfo = unitInfoIdMap.get(unitId);
+    if (!unitInfo) {
+      return;
+    }
+    ret.set(name, unitInfo);
+  });
+
+  return ret;
 };
