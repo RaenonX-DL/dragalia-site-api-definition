576c77f7f1d7748e3848c0b7acedf7fb3024ca6c
RaenonX <raenonx0710@gmail.com>
2021-07-14 06:31:40 -0500

    ADD - Simple unit info `icon` field
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- resources/types/simpleInfo.ts
+++ resources/types/simpleInfo.ts
@@ -2,7 +2,8 @@ import {MultiLangText} from './common/text';
 
 
 export type SimpleUnitInfoEntry = {
-  name: MultiLangText
+  name: MultiLangText,
+  icon: string,
 }
 
 export type SimpleUnitInfo = Record<string, SimpleUnitInfoEntry> // Key is the unit ID
