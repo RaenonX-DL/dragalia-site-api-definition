a87a7a7a2533f52ca25ab084604f4f139f8256d2
RaenonX <raenonx0710@gmail.com>
2022-01-14 16:44:27 -0600

    Added common env checking utils
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ utils.ts
@@ -0,0 +1,21 @@
+export const isProduction = () => {
+  return process.env.NODE_ENV === 'production';
+};
+
+export const isCi = () => {
+  const ciString = process.env.CI;
+
+  if (!ciString) {
+    return false;
+  }
+
+  if (ciString === '1' || ciString.toLowerCase() === 'true') {
+    return true;
+  }
+
+  if (ciString === '0' || ciString.toLowerCase() === 'false') {
+    return false;
+  }
+
+  throw Error(`Environment variable \`CI\` must be defined as either 0, 1, true, false. However, it is ${ciString}`);
+};
